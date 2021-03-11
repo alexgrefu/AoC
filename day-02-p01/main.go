f33def1163667129c43029666a88a8610eff30b9
Alex Grefu <alex.grefu@jlg.ro>
2021-03-12 01:50:42 +0200

    day two, part one

--- day-02-p01/main.go
+++ day-02-p01/main.go
@@ -1,1 +1,46 @@
-package day_02_p01
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+
+
+func main() {
+	fmt.Println("valid passwords: ", countValidPasswords("input.txt"))
+}
+
+func countValidPasswords(fileName string) int {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	s := bufio.NewScanner(file)
+	var valid int
+	for s.Scan() {
+		var min, max int
+		var letter rune
+		var passwd string
+
+		if _, err := fmt.Sscanf(s.Text(), "%d-%d %c: %s", &min, &max, &letter, &passwd); err != nil {
+			log.Fatal(err)
+		}
+		r := record{min, max, letter, make(map[rune]int)}
+		for _, l := range passwd {
+			r.Password[l]++
+		}
+
+		if r.isValid() { valid++ }
+	}
+
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return valid
+}
+
+
--- day-02-p01/record.go
+++ day-02-p01/record.go
@@ -1,1 +1,17 @@
 package main
+
+type record struct {
+	Min int
+	Max int
+	Letter rune
+	Password map[rune]int
+}
+
+func (r *record) isValid() bool{
+	if n, ok := r.Password[r.Letter]; ok {
+		if r.Min <= n && n <= r.Max {
+			return true
+		}
+	}
+	return false
+}
